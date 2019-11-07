80be3a91dd2a16028e5e992960b3e508604abb13
Ricardo Balbinot <ricardo.balbinot@ciss.com.br>
2019-11-07 13:46:25 -0300

    Simplificação do db.json

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -10,7 +10,7 @@ export default function Main() {
  
   renderJogos = ({ item }) => (
     <View>
-      <Text >Adversário: {item.away_name}</Text>
+      <Text >Adversário: {item.adversario}</Text>
       <Text >Jogo: {item.status}</Text>
       <TouchableOpacity>
         <Text>Ver mais</Text>
